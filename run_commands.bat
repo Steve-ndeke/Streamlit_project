47789ecb6554f829f533fad7bd55d96d20fb6674
steve-ndeke <stevendeke@gmail.com>
2024-09-02 23:29:12 +0300

    added setup tools in the bat file

--- run_commands.bat
+++ run_commands.bat
@@ -1,6 +1,25 @@
+@REM @echo off
+@REM python -m venv myenv
+@REM myenv\Scripts\activate
+@REM python -m pip install --upgrade pip
+@REM pip install setuptools
+@REM python -m pip install -r requirements.txt
+
 @echo off
 python -m venv myenv
-myenv\Scripts\activate
+call myenv\Scripts\activate
+
+REM Upgrade pip to ensure you have the latest version
 python -m pip install --upgrade pip
-pip install setuptools
+
+REM Install setuptools, which is needed for many Python packages
+pip install --upgrade setuptools
+
+REM Install all the requirements for your project
 python -m pip install -r requirements.txt
+
+REM Check if distutils is available and attempt to install if not
+pip install distlib
+
+REM Start the Streamlit app
+streamlit run 00_🚪_Gateway.py
